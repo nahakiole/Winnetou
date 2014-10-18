b2185510f4fc018bba7784f0fe0f1de6d5f0ea73
Nahakiole <robin.glauser@gmail.com>
2014-10-18 23:56:16 +0200

    Adding icon.

--- src/ch/robinglauser/winnetou/ControlCenter.java
+++ src/ch/robinglauser/winnetou/ControlCenter.java
@@ -1,33 +1,65 @@
 package ch.robinglauser.winnetou;
 
+
+import com.apple.eawt.Application;
+
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 
-public class ControlCenter extends JFrame {
+public class ControlCenter extends JFrame implements ActionListener {
 
+    JLabel apacheStatus = new JLabel("Not running");
+    JButton startApache = new JButton("Start Apache");
 
-    public static void main(String[] args) {
-        ControlCenter controlCenter = new ControlCenter();
-        controlCenter.setTitle("Winnetou");
-        JPanel startPanel = new JPanel(new GridLayout(2,2,5,5));
+    JLabel mysqlStatus = new JLabel("Not running");
+    JButton startMySQL = new JButton("Start MySQL");
 
-        startPanel.setBorder(new EmptyBorder(10, 10, 10, 10) );
-        JLabel apacheStatus = new JLabel("Not running");
-        JButton startApache = new JButton("Start Apache");
+    public ControlCenter() {
+        setTitle("Winnetou");
+        setIconImage(Toolkit.getDefaultToolkit().getImage("winnetou.png"));
+        JPanel startPanel = new JPanel(new GridLayout(2, 2, 5, 5));
 
-        JLabel mysqlStatus = new JLabel("Not running");
-        JButton startMySQL = new JButton("Start MySQL");
+        startPanel.setBorder(new EmptyBorder(10, 10, 10, 10));
+
         startPanel.add(apacheStatus);
         startPanel.add(startApache);
         startPanel.add(mysqlStatus);
         startPanel.add(startMySQL);
-        controlCenter.add(startPanel);
+        startMySQL.addActionListener(this);
+        startApache.addActionListener(this);
+        add(startPanel);
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        pack();
+        setVisible(true);
 
 
-        controlCenter.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        controlCenter.pack();
-        controlCenter.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("Started");
+        Application application = Application.getApplication();
+        Image image = Toolkit.getDefaultToolkit().getImage("resources/winnetou.png");
+        application.setDockIconImage(image);
+
+        ControlCenter controlCenter = new ControlCenter();
+
+
+    }
+
+
+    public void actionPerformed(ActionEvent evt) {
+        System.out.println("Clicked");
+        Object src = evt.getSource();
+        if (src == startApache) {
+
+        } else if (src == startMySQL) {
+
+        }
     }
 }
